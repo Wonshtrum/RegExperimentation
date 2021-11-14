f9f7ee9d6ba96fa2676290b76c1a7ab29ae35537
Wonshtrum <pygmee64@gmail.com>
2021-11-14 16:05:54 +0100

    Proper character range class

--- regex.py
+++ regex.py
@@ -1,5 +1,9 @@
+from time import time
+
+
 NOT_MATCH = False
 HAS_MATCH = True
+EPSILON = None
 
 
 def append(elements, element):
@@ -30,16 +34,70 @@ class Range:
 class Ranges:
 	min_char = 0
 	max_char = 127
-	def __init__(self, *ranges):
-		self.ranges = ranges
+	def __init__(self, *ranges, inverted=False):
+		ranges = sorted((_, _) if isinstance(_, int) else _ for _ in ranges)
+		self.ranges = []
+		last = Ranges.min_char-1
+		for min_char, max_char in ranges:
+			if min_char <= last:
+				min_char, old_max_char = self.ranges.pop()
+				max_char = max(max_char, old_max_char)
+			self.ranges.append((min_char, max_char))
+			last = max_char+1
+
+		if inverted:
+			self.ranges = self.star.intersect(self)[0]
 
 	def intersect(self, other):
 		in_self = []
 		in_both = []
 		in_other = []
 		i = 0
-		while True:
-			range_self = self.ranges[i]
+		j = 0
+		get = lambda l, i: l[i] if i < len(l) else (Ranges.max_char+1, Ranges.max_char+1)
+		self_min, self_max = get(self.ranges, i)
+		other_min, other_max = get(other.ranges, j)
+		while i < len(self.ranges) or j < len(other.ranges):
+			if self_max < other_min:
+				i += 1
+				in_self.append((self_min, self_max))
+				self_min, self_max = get(self.ranges, i)
+				continue
+			if other_max < self_min:
+				j += 1
+				in_other.append((other_min, other_max))
+				other_min, other_max = get(other.ranges, j)
+				continue
+
+			if self_min < other_min:
+				in_self.append((self_min, other_min-1))
+				self_min = other_min
+			if other_min < self_min:
+				in_other.append((other_min, self_min-1))
+				other_min = self_min
+
+			if self_max < other_max:
+				i += 1
+				in_both.append((self_min, self_max))
+				other_min = self_max+1
+				self_min, self_max = get(self.ranges, i)
+			elif other_max < self_max:
+				j += 1
+				in_both.append((other_min, other_max))
+				self_min = other_max+1
+				other_min, other_max = get(other.ranges, j)
+			else:
+				i += 1
+				j += 1
+				in_both.append((self_min, self_max))
+				self_min, self_max = get(self.ranges, i)
+				other_min, other_max = get(other.ranges, j)
+		return in_self, in_other, in_both
+
+	def __repr__(self):
+		return '['+','.join(f'{min_char}' if min_char==max_char else f'{min_char}-{max_char}' for min_char, max_char in self.ranges)+']'
+
+Ranges.star = Ranges((Ranges.min_char, Ranges.max_char))
 
 
 class Atom(Regex):
@@ -68,7 +126,7 @@ class Repeat(Regex):
 	def advance(self):
 		result = []
 		if not self.dirty and self.count == self.min == 0:
-			result.append((None, HAS_MATCH, self.copy()))
+			result.append((EPSILON, HAS_MATCH, self.copy()))
 		sub_exprs = self.expr.advance()
 		for path, status, sub_expr in sub_exprs:
 			copy = self.copy()
@@ -80,7 +138,7 @@ class Repeat(Regex):
 				if copy.count == self.max:
 					result.append((path, HAS_MATCH, copy))
 					continue
-				if path is None:
+				if path is EPSILON:
 					result.extend(copy.advance())
 					continue
 				elif copy.count >= self.min:
@@ -149,7 +207,7 @@ class Sequence(Regex):
 	def advance(self):
 		result = []
 		if self.cursor == len(self.exprs):
-			result.append((None, HAS_MATCH, self))
+			result.append((EPSILON, HAS_MATCH, self))
 			return result
 		sub_exprs = self.exprs[self.cursor].advance()
 		for path, status, sub_expr in sub_exprs:
@@ -160,7 +218,7 @@ class Sequence(Regex):
 			if copy.cursor == len(self.exprs):
 				result.append((path, HAS_MATCH, copy))
 				continue
-			if path is None:
+			if path is EPSILON:
 				result.extend(copy.advance())
 				continue
 			result.append((path, NOT_MATCH, copy))
@@ -209,7 +267,6 @@ class Family(Regex):
 
 
 def merge(graph, i, j, replace=False):
-	print("merge", i, j)
 	if replace:
 		graph[j] = graph[i]
 	graph[i] = []
@@ -228,7 +285,7 @@ def _compile(graph, state=0):
 		for j, (expr, transition, *_) in enumerate(exprs):
 			sub_exprs = expr.advance()
 			for path, status, sub_expr in sub_exprs:
-				if path is None:
+				if path is EPSILON:
 					transition[path] = -1
 					continue
 				path = path.char_min
@@ -241,7 +298,7 @@ def _compile(graph, state=0):
 					graph.append(line)
 				if all(sub_expr != other for other, *_ in line):
 					if status == HAS_MATCH:
-						line.append([sub_expr, {None:-1}])
+						line.append([sub_expr, {EPSILON:-1}])
 					else:
 						line.append([sub_expr, {}])
 				transition[path] = transitions[path] = state
@@ -256,22 +313,27 @@ def _compile(graph, state=0):
 				state -= 1
 				graph.pop()
 				break
-
-	for i, state in enumerate(graph):
-		print(i)
-		for expr, transition, *_ in state:
-			pass
-			print(expr, transition)
-		print()
 	return stop
 
 
 def compile(graph):
 	last = None
 	state = 0
+	t = time()
 	while state != last:
 		last = state
 		state = _compile(graph, last)
+	t = time()-t
+	print_graph(graph)
+	print(t)
+
+
+def print_graph(graph):
+	for i, state in enumerate(graph):
+		print(i)
+		for expr, transition, *_ in state:
+			print(expr, transition)
+		print()
 
 
 def make_graph(*exprs):
